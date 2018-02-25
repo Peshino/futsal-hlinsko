d8485bc4eaf56fdc6e83043b23543427b6a84edd
Jiri Pesek <pesek.jirka@centrum.cz>
2018-02-25 18:55:04 +0100

    Laravel 5.4 From Scratch: Layouts and Structure

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+  <h1>Place to show the post</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 Route::get('/tasks', 'TasksController@index');
 
 Route::get('/tasks/{task}', 'TasksController@show');
+
+Route::get('posts', 'PostsController@index');
+
+Route::get('posts/{post}', 'PostsController@show');
