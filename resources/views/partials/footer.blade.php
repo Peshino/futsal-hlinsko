f24295d663d7e4382537848d65eb92dda6db593f
Peshino <pesek.jirka@centrum.cz>
2022-11-01 11:27:05 +0100

    footer mailto added to created by

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,13 +6,15 @@
                 @lang('messages.app_name')
             </a>
             | @lang('messages.footer_rights')
-            | @lang('messages.created_by') <a id="created-by" href="" data-toggle="popover" title="Jiří Pešek"
+            | @lang('messages.created_by') <a id="created-by" href="mailto:pesek.jirka@centrum.cz" data-toggle="popover"
+                title="Jiří Pešek"
                 data-content='
                 <div class="text-left">
                     <i class="fas fa-phone"></i>&nbsp;&nbsp;&nbsp;+420 721 455 588<br />
                     <i class="far fa-envelope"></i>&nbsp;&nbsp;&nbsp;pesek.jirka<i class="fas fa-at"></i>centrum.cz
                 </div>
-            '>Jiří Pešek</a>
+            '>Jiří
+                Pešek</a>
         </small>
     </div>
 </footer>
