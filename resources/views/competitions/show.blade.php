50f77e8207de544c50bc358a9e9d77fbbd27904b
Jiří Pešek <pesek.jirka@centrum.cz>
2024-02-20 19:36:22 +0100

    medals to specific competition added

--- resources/views/competitions/show.blade.php
+++ resources/views/competitions/show.blade.php
@@ -67,48 +67,55 @@
                         </div>
                     </div> --}}
 
-                    @if ($competition->id === 18)
-                        <div class="row mb-4 game">
-                            <div class="col no-padding">
-                                <div class="pt-2 pb-2 first-place text-center">
-                                    <h3>
-                                        @if ($competition->id === 17)
-                                        @endif
+                    <div class="row mb-4 game">
+                        <div class="col no-padding">
+                            <div class="pt-2 pb-2 first-place text-center">
+                                <h3>
+                                    @if ($competition->id === 17)
+                                        <a href="{{ route('teams.show', [$competition->id, 186]) }}">
+                                            <i class="fas fa-medal"></i> Forza Hlinsko
+                                        </a>
+                                    @endif
 
-                                        @if ($competition->id === 18)
-                                            <a href="{{ route('teams.show', [$competition->id, 177]) }}">
-                                                <i class="fas fa-medal"></i> Hattrick Svratka
-                                            </a>
-                                        @endif
-                                    </h3>
-                                </div>
-                                <div class="pt-2 pb-2 second-place text-center">
-                                    <h5>
-                                        @if ($competition->id === 17)
-                                        @endif
+                                    @if ($competition->id === 18)
+                                        <a href="{{ route('teams.show', [$competition->id, 177]) }}">
+                                            <i class="fas fa-medal"></i> Hattrick Svratka
+                                        </a>
+                                    @endif
+                                </h3>
+                            </div>
+                            <div class="pt-2 pb-2 second-place text-center">
+                                <h5>
+                                    @if ($competition->id === 17)
+                                        <a href="{{ route('teams.show', [$competition->id, 189]) }}">
+                                            <i class="fas fa-medal"></i> Tatra tým Hlinsko
+                                        </a>
+                                    @endif
 
-                                        @if ($competition->id === 18)
-                                            <a href="{{ route('teams.show', [$competition->id, 191]) }}">
-                                                <i class="fas fa-medal"></i> Atlético Juniors
-                                            </a>
-                                        @endif
-                                    </h5>
-                                </div>
-                                <div class="pt-2 pb-2 third-place text-center">
-                                    <h5>
-                                        @if ($competition->id === 17)
-                                        @endif
+                                    @if ($competition->id === 18)
+                                        <a href="{{ route('teams.show', [$competition->id, 191]) }}">
+                                            <i class="fas fa-medal"></i> Atlético Juniors
+                                        </a>
+                                    @endif
+                                </h5>
+                            </div>
+                            <div class="pt-2 pb-2 third-place text-center">
+                                <h5>
+                                    @if ($competition->id === 17)
+                                        <a href="{{ route('teams.show', [$competition->id, 184]) }}">
+                                            <i class="fas fa-medal"></i> AC Hlinsko
+                                        </a>
+                                    @endif
 
-                                        @if ($competition->id === 18)
-                                            <a href="{{ route('teams.show', [$competition->id, 185]) }}">
-                                                <i class="fas fa-medal"></i> FC Ještě letos
-                                            </a>
-                                        @endif
-                                    </h5>
-                                </div>
+                                    @if ($competition->id === 18)
+                                        <a href="{{ route('teams.show', [$competition->id, 185]) }}">
+                                            <i class="fas fa-medal"></i> FC Ještě letos
+                                        </a>
+                                    @endif
+                                </h5>
                             </div>
                         </div>
-                    @endif
+                    </div>
 
                     @if ($gameCurrentlyBeingPlayed !== null || $firstSchedule !== null || $lastResult !== null)
                         <div class="justify-content-center border-bottom border-dark mb-2 pb-3">
