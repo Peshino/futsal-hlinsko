03618847c5dbeefcc3057bcea1dc9e933c6987ad
Peshino <pesek.jirka@centrum.cz>
2024-11-23 19:27:02 +0100

    team colors added to game rows, also t-shirt alert

--- resources/views/competitions/show.blade.php
+++ resources/views/competitions/show.blade.php
@@ -17,6 +17,13 @@
         <div class="card-body no-x-padding">
             <div class="content">
                 <div class="content-block">
+                    tady
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 640 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path
+                            d="M211.8 0c7.8 0 14.3 5.7 16.7 13.2C240.8 51.9 277.1 80 320 80s79.2-28.1 91.5-66.8C413.9 5.7 420.4 0 428.2 0l12.6 0c22.5 0 44.2 7.9 61.5 22.3L628.5 127.4c6.6 5.5 10.7 13.5 11.4 22.1s-2.1 17.1-7.8 23.6l-56 64c-11.4 13.1-31.2 14.6-44.6 3.5L480 197.7 480 448c0 35.3-28.7 64-64 64l-192 0c-35.3 0-64-28.7-64-64l0-250.3-51.5 42.9c-13.3 11.1-33.1 9.6-44.6-3.5l-56-64c-5.7-6.5-8.5-15-7.8-23.6s4.8-16.6 11.4-22.1L137.7 22.3C155 7.9 176.7 0 199.2 0l12.6 0z" />
+                    </svg>
+                    <i class="fa-solid fa-shirt text-white"></i>
                     {{-- <h3>Zde na homepage soutěže by mohlo být:</h3>
                     <ul class="pl-5">
                         <li>důležitá upozornění od administrátora, novinky přidané do aplikace</li>
@@ -127,7 +134,7 @@
                                 @php
                                     $game = $gameCurrentlyBeingPlayed;
                                 @endphp
-                                <div class="row">
+                                <div class="row no-margin">
                                     <div class="col no-padding">
                                         @include('partials/game')
                                     </div>
@@ -145,7 +152,7 @@
                                     $game = $firstSchedule;
                                     $startDateTime = \Carbon\Carbon::parse($game->start_datetime);
                                 @endphp
-                                <div class="row">
+                                <div class="row no-margin">
                                     <div class="col no-padding">
                                         <div class="text-center schedule-color">
                                             @lang('messages.next_schedule') | <small>{{ $startDateTime->isoFormat('Do[.] MMMM') }}</small>
@@ -160,7 +167,7 @@
                                     $game = $lastResult;
                                     $startDateTime = \Carbon\Carbon::parse($game->start_datetime);
                                 @endphp
-                                <div class="row">
+                                <div class="row no-margin">
                                     <div class="col no-padding">
                                         <div class="text-center competition-color">
                                             @lang('messages.last_result') |
